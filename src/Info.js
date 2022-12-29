9aefeb32b932ece90e2f687b521818164fa94997
Karim Ibrahim <chillman359@gmail.comm>
2022-12-29 16:35:47 -0500

    First commit

---
+++ src/Info.js
@@ -0,0 +1,25 @@
+// Importing the css for the info
+import "./css/info.css";
+
+const Info = ({elapsedTurn}) => {
+
+	return (
+        <div>
+            <div className="info">
+                <div className="text">Player 1: X</div>
+                <div className="text">Player 2: O</div>
+            </div>
+            <div className="info">
+                <div className="text">Turn : {elapsedTurn}</div>
+            </div>
+        </div>
+	)
+}
+
+/*
+            <div className="info">
+                <div className="text">Turn : {elapsedTurn}</div>
+            </div>
+            */
+
+export default Info;
